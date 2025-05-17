37887612e955b812260c30b30bad2d2c9991f3df
Mohd Zain <zainmohd@sigmoidanalytics.com>
2025-05-17 22:08:03 +0530

    update data loading for parquet file

--- core/data_loader.py
+++ core/data_loader.py
@@ -6,7 +6,8 @@ class Data:
     def __init__(self):
         print("loading data in data_loader")
         self.data_description = pd.read_csv("./data/origination_feats.csv")
-        self.data = pd.read_excel("./data/morigination.xlsx", sheet_name="Sheet1")
+        # self.data = pd.read_excel("./data/morigination.xlsx", sheet_name="Sheet1")
+        self.data = pd.read_parquet("./data/origination.parquet")
         print("data loaded in data loader")
 
     @staticmethod
